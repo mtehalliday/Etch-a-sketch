4f0228650f64c19688b765bb3a0bf38e74bb0ad0
Matthew Halliday <matthalliday54@hotmail.com>
2023-06-26 20:24:16 +0100

    Fix errors preventing div drawing

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
-const container = document.querySelector(#container);
+const container = document.body.querySelector("#container");
+
+//First create 16 column divs, divs with class “col”, and Id with a number from 1 – 16, and then 
+//append then, create a loop which adds 16, pixel divs to each columns, so create them, then append. 
+//Finally append to container div 
+
 
 for (let i = 0; i < 16; i++) {
-    let div_col = createElement("div");
+    let div_col = document.createElement("div");
     div_col.classList.add(`col_{i}`);
     for (let j = 0; j < 16; j++) {
-        let row = createElement("div");
+        let row = document.createElement("div");
         div_col.appendChild(row);
     }
 }
