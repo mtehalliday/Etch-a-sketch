f7a9fce255fd341b12c41df81ed4d1942dff44b4
Matthew Halliday <matthalliday54@hotmail.com>
2023-06-27 11:28:25 +0100

    refactor and create grid function

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
-const container = document.body.querySelector("#container");
+const container = document.body.querySelector(".container");
 
 //First create 16 column divs, divs with class “col”, and Id with a number from 1 – 16, and then 
 //append then, create a loop which adds 16, pixel divs to each columns, so create them, then append. 
 //Finally append to container div 
 
-
-for (let i = 0; i < 16; i++) {
-    let div_col = document.createElement("div");
-    div_col.classList.add(`col_{i}`);
-    for (let j = 0; j < 16; j++) {
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
         let row = document.createElement("div");
-        div_col.appendChild(row);
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < 16; j++) {
+            let column = document.createElement("div");
+            column.classList.add("col")
+            row.appendChild(column);
+        }
     }
 }
 
-
-const gridSquare = document.createElement("div");
-gridSquare.classList.add("square");
+createGrid();
